1278240dca53432f4f8ca6241e1a25778ee2bbba
Ptitlu42 <118479475+Ptitlu42@users.noreply.github.com>
2023-01-18 23:14:52 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,27 +1,46 @@
-# Gère le compte courant et les action liées à celui-ci
-class Current_account:
+class Account:
+    def __init__(self, balance: int, owner: str, agios: int): # Définition de Account
+        balance = self.balance # Solde
+        owner = self.owner # Propriétaire
+
+
+class Current_account: # Definition du compte courant avec l'héritage de Account
+    def __init__(Account, authorized_overdraft, amount): # Initialisation des éléments liés au compte courant   
+        Account.authorized_overdraft = authorized_overdraft # Découvert autorisé
+        Account.amount = amount # Montant de la transaction
     
-    # Initialisation des éléments lié au compte courant
-    def __init__(self, actual_value: int, authorized_overdraft: int, transaction: int):   
-        self.actual_value = actual_value
-        self.authorized_overdraft = authorized_overdraft
-        self.transaction = transaction
     
-    # Gère les ajouts sur le compte
-    def __add__(self, value_to_add: int):
-        pass
-    
-    # Gère les retraits sur le compte
-    def __sub__(self, value_to_sub: int):
-        pass
-    
-    # Gère les agios si le solde est en dessous du découvert après un retrait
-    def __more_than_authorized__(self, penalty_value: int):
-        pass
-    
-    def __str__(self, transaction, actual_value) -> str:
-        return "Vous avez effecter l'opération {}. \n Voici le solde de votre compte: {}." .\
-            format (transaction, actual_value)
+    def __payment__(Account, amount): # Gère les versements sur le compte
+        balance = balance + amount
+        print ("Versement de {} effectué. \n Solde actuel: {}") .\
+            format (amount, balance)
+            
+    def __withdraw__(Account, amount): # Gère les retraits sur le compte 
+        balance = balance - amount
+        print ("Retrait de {} effectué. \n Solde actuel: {}") .\
+            format (amount, balance)
+            
+    def __withdraw_with_depassed_overdraft__ (Account, amount): # Gère les retrait si le solde dépasse le découvert autorisé après le retrait      
+          balance = Account.balance
+          agios = Account.agios
+          value_agios = amount * (agios / 100) # Calcule le montant de taxe agios qui va être prelever en plus du retrait
+          balance = balance - amount - value_agios # Calcule le sole après le retrait et après les agios
+          print ("Vous avez effectuer un retrait de {}. Des agios d'une valeur de {} ({}%) ont été appliqués. \n Solde actuel: {}.") .\
+              format(amount, value_agios, agios, balance)
+          
+          
+    def __consult__(Account, balance, authorized_overdraft, owner): #Gère la partit pour consulter le compte
+        balance = Account.balance
+        authorized_overdraft = Account.authorized_overdraft
+        owner = Account.owner
+        print ("Voici les informations concernant votre compte commun: \n Propriétaire: {}. \n Solde: {}$. \n Découvert authorisé: {}.") .\
+            format(owner, balance, authorized_overdraft)
+            
         
         
+        
+        
+    
+    
+    
             
