077df72bf9cb44aa3a39c1c7cd298a1e4c96d943
Ptitlu42 <118479475+Ptitlu42@users.noreply.github.com>
2023-01-18 23:14:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from objects import *
 import time
+from objects import *
 
 print("Bienvenue dans la P'tit Lu's BANK!")
 
@@ -7,24 +7,28 @@ while True:
     choice = input("Que souhaitez-vous faire? \n 1: Un dépôt. \n 2: Un retrait. \n 3: Consulter mes comptes. \n 4: Effectuer un virement. \n 5: Quitter. \n")
 
     if choice == "1":
-        transaction = int(input("Quel montant voulez-vous déposer? \n"))
+        amount = int(input("Quel montant voulez-vous déposer? \n"))
         # Code pour effectuer le dépôt ici
-        print("Dépôt effectué avec succès!")
-        
+        time.sleep(8)
+        break
+    
     elif choice == "2":
-        transaction = int(input("Quel montant souhaitez-vous retirer? \n"))
+        amount = int(input("Quel montant souhaitez-vous retirer? \n"))
         # Code pour effectuer le retrait ici
-        print("Retrait effectué avec succès!")
-        
+        time.sleep(8)
+        break
+       
     elif choice == "3":
-        account_choice = input("Quel compte souhaitez-vous consulter? \n")
+        account = input("Quel compte souhaitez-vous consulter? \n")
         # Code pour afficher les informations du compte ici
         
     elif choice == "4":
         transfer_type = input("1: Compte courant vers compte épargne. \n 2: Epargne vers compte courant. \n")
         # Code pour effectuer le virement ici
         print("Virement effectué avec succès!")
-        
+        time.sleep(8)
+        break
+    
     elif choice == "5":
         print("Au revoir!")
         time.sleep(3)  
