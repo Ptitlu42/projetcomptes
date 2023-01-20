1bd0aa3735190f5a6ea146dea7706172711406de
Ptitlu42 <118479475+Ptitlu42@users.noreply.github.com>
2023-01-20 18:47:17 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,56 +1,88 @@
+import time
+
+
 class Account:
-    def __init__(self, balance: float, owner: str, agios: float, interest: int): # Définition de Account
-        self.balance = balance # Solde
+    def __init__(self, balance: int ,owner: str ,agios: int ,interest: int, authorized_overdraft: int, amount: int, saving_balance: int, saving_interest: int):
+        self.balance = balance
         self.owner = owner
+        self.agios = agios
         self.interest = interest
-        self.agios = agios# Propriétaire
+        self.authorized_overdraft = authorized_overdraft
+        self.amount = amount
+        self.saving_balance = saving_balance
+        self.saving_interest = saving_interest
+                
+class Current_account(Account): 
+    def __init__(self, balance, owner, agios, interest, authorized_overdraft, amount, saving_balance):
+        super.__init__(self, balance, owner, agios, interest, authorized_overdraft, amount, saving_balance)
         
-    
-
-
-class Current_account: # Definition du compte courant avec l'héritage de Account
-    def __init__(Account, balance: float, owner: str, agios: float, authorized_overdraft, amount): # Initialisation des éléments liés au compte courant   
-        Account.authorized_overdraft = authorized_overdraft # Découvert autorisé
-        Account.amount = amount # Montant de la transaction
-        Account.balance = balance
-        Account.owner = owner
-        Account.agios = agios
+    def withdraw(self, amount):
+        amount = int(input("Quel montant voulez-vous retirer? \n")) 
+               
+        if (self.balance - amount) <= self.authorized_overdraft:
+            print("Découvert autorisé dépassé. \n Solde:{}$." .\
+                format (self.balance))
+            time.sleep(4)
         
-    def __payment__(self, amount): # Gère les versements entre les comptes
-            self.balance += amount
-            print ("Versement de {}$ effectué. \n Solde actuel: {}$".\
-                format(amount, self.balance))
-             
-    def __withdraw__(self, amount): # Gère les retraits sur le compte       
-        self.balance = self.balance - amount
-        print ("Retrait de {}$ effectué. \n Solde actuel: {}$" .\
-            format (amount, self.balance))
-            
-    def __withdraw_with_depassed_overdraft__ (self, amount): # Gère les retrait si le solde dépasse le découvert autorisé après le retrait      
-          self.agios = self.agios
-          value_agios = amount * (self.agios / 100) # Calcule le montant de taxe agios qui va être prelever en plus du retrait
-          self.balance = self.balance - amount - value_agios # Calcule le sole après le retrait et après les agios
-          print ("Vous avez effectuer un retrait de {}$. Des agios d'une valeur de {} ({}%) ont été appliqués. \n Solde actuel: {}$." .\
+        else:
+            self.balance = (self.balance - amount)
+            print ("Vous avez effectuer un retrait de {}$.\n Solde actuel: {}$." .\
+                format (amount, self.balance))
+            time.sleep(4)
+                                        
+        if (self.balance - amount) < 0 and (self.balance - amount) > self.authorized_overdraft:
+            value_agios = amount * (self.agios / 100)
+            self.balance = self.balance - amount - value_agios
+            print ("Vous avez effectuer un retrait de {}$. Des agios d'une valeur de {} ({}%) ont été appliqués. \n Solde actuel: {}$." .\
               format(amount, value_agios, self.agios, self.balance))
-          
-          
-    def __consult_current__(self): #Gère la partit pour consulter le compte
-        self.balance = self.balance
-        self.authorized_overdraft = self.authorized_overdraft
-        self.owner = self.owner
-        print ("Voici les informations concernant votre compte commun: \n Propriétaire: {}. \n Solde: {}$. \n Découvert authorisé: {}$." .\
-            format(self.owner, self.balance, self.authorized_overdraft)) 
+            time.sleep(4)
+                        
+    def payment(self, amount):
+        amount = int(input("Quel montant souhaitez vous déposer ? \n"))
+        interest_value = amount * (self.interest / 100)
+        self.balance = self.balance + amount + interest_value
+        print ("Vous avez effectuer un depot de {}$. \n Vos interets: {}$ ({}%). \n Solde actuel: {}$." .\
+            format (amount, interest_value, self.interest, self.balance))
+        time.sleep(4)
+            
+    def consult_current(self):
+        print ("Voici les informations concernant votre compte commun: \n \n Propriétaire: {}. \n Solde: {}$. \n Découvert autorisé: {}$. \n Intérêts: {}%." .\
+            format(self.owner, self.balance, self.authorized_overdraft, self.interest))
+        time.sleep(4)  
         
-class Saving_account: # Definition du compte courant avec l'héritage de Account
-    def __init__(Account, balance: float, owner: str, agios: float, amount, interest: int): # Initialisation des éléments liés au compte courant   
-        Account.amount = amount # Montant de la transaction
-        Account.balance = balance
-        Account.owner = owner
-        Account.agios = agios
+    def transfer_to_saving(self, amount):
+        amount = int(input("Quel motant souhaitez vous versé sur votre compte épargne ? \n"))
+        if self.balance - amount < self.authorized_overdraft:
+            print ("Vous ne pouvez pas effectuer ce virement, votre compte courant dépasserai la limite autorisé. \n Solde compte courant: {}$. \n Solde compte épargne: {}$." .\
+                format (self.balance, self.saving_balance))
+            time.sleep(4)
+        else:
+            self.balance = self.balance - amount
+            self.saving_balance = self.balance + amount
+            print ("Votre virement a bien été pris en compte. \n Solde compte courant: {}$. \n Solde compte épargne: {}$." .\
+                format (self.balance, self.saving_balance))
+            time.sleep(4)
+            
+class Saving_account(Account):
+    def __init__(self, balance, owner, agios, authorized_overdraft, interest, amount, saving_balance, saving_interest):
+        super.__init__(self, balance, agios, owner, authorized_overdraft, interest, amount, saving_balance, saving_interest)
+    
+    def consult_saving(self):
+        print ("Voici les informations concernant votre compte commun: \n \n Propriétaire: {}. \n Solde: {}$.\n Intérêts: {}%." .\
+            format(self.owner, self.saving_balance, self.saving_interest))
+        time.sleep(4) 
         
-    def __consult_saving__(self):
-        self.interest = self.interest#Gère la partit pour consulter le compte
-        self.balance = self.balance
-        self.owner = self.owner
-        print ("Voici les informations concernant votre compte épargne: \n Propriétaire: {}. \n Taux d'intéret: {}%. \n Solde: {}$." .\
-            format(self.owner, self.balance,)) 
+    def transfer_to_current(self, amount):
+        amount = int(input("Quel motant souhaitez vous versé sur votre compte courant ? \n"))
+        if self.saving_balance - amount < 0:
+            print ("Vous ne pouvez pas effectuer ce virement, votre compte épargne dépasserai la limite autorisé. \n Solde compte courant: {}$. \n Solde compte épargne: {}$." .\
+                format (self.balance, self.saving_balance))
+            time.sleep(4)
+        else:
+            self.saving_balance = self.saving_balance - amount
+            self.balance = self.balance + amount
+            print ("Votre virement a bien été pris en compte. \n Solde compte courant: {}$. \n Solde compte épargne: {}$." .\
+                format (self.balance, self.saving_balance))
+            time.sleep(4)
+    
+        
