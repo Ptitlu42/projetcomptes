4561e2bb2be508d47593eda5f4a76ec1bc06d95c
Ptitlu42 <118479475+Ptitlu42@users.noreply.github.com>
2023-01-20 18:46:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
 import time
 from objects import *
 
-balance = 0
-owner = "P'tit Lu"
+balance = 1000
+owner = "Erwann DuCloture-TonCompte"
 agios = 10
 authorized_overdraft = -200
 amount = ()
 interest = 5
+saving_balance = 4000
+saving_interest = 10
 
-account = Current_account(balance, owner, agios, authorized_overdraft, amount)
+account = Account(balance, owner, agios,interest, authorized_overdraft,amount, saving_balance, saving_interest)
 
-print("Bienvenue dans la P'tit LuCrative (banque à but Lucratif)")
+print("Bienvenue dans la P'tit LuCrative")
 
 while True:
-    choice = input("Que souhaitez-vous faire? \n 1: Un dépôt. \n 2: Un retrait. \n 3: Consulter mes comptes. \n 4: Effectuer un virement. \n 5: Quitter. \n")
-
-    if choice == "1":
-        amount = float(input("Quel montant voulez-vous déposer? \n"))
-        account.__payment__(amount)
-        time.sleep(4)
-            
-    elif choice == "2":
-        amount = float(input("Quel montant souhaitez-vous retirer? \n"))
-        if (balance - amount) < (authorized_overdraft):
-            account.__withdraw_with_depassed_overdraft__(amount)
-            time.sleep(4)
-        else:
-            account.__withdraw__(amount)
-            time.sleep(4)
-              
-    elif choice == "3":
-        account_choice = input("Quel compte souhaitez-vous consulter? \n 1: Compte courant. \n 2: Compte épargne. \n Quitter.\n")
-        while True:
-            if account_choice == "1":
-                account.__consult_current__()
-                time.sleep(8)
-                break
-            elif account_choice == "2":
-                account.__consult_saving__()
-                time.sleep(8)
-                break
-            elif account_choice == "3":
-                print ("A bientôt chez P'tit LuCrative!")
-                time.sleep(4)
-       
-    if choice == "4":
-        transfer_type = input("1: Compte courant vers compte épargne. \n 2: Epargne vers compte courant. \n")
-        # Code pour effectuer le virement ici
-        print("Virement effectué avec succès!")
-        time.sleep(8)
-        
+    choice = input("Que souhaitez-vous faire? \n 1: Un retrait. \n 2: Un dépot. \n 3: Consulter mes comptes. \n 4: Effectuer un virement. \n 5: Quitter. \n")
     
-    if choice == "5":
-        print("A bientôt chez P'tit LuCrative!")
-        time.sleep(2)  
-        break
+    if choice == "1": #ok
+        Current_account.withdraw(account, amount)
+    
+    if choice == "2": #ok
+        Current_account.payment(account, amount)
         
-    else:
-        print("ERREUR: Veuillez choisir l'une des propositions: \n 1: pour un dépôt. \n 2: pour un retrait. \n 3: pour consulter vos comptes. \n 4: pour effectuer un virement. \n 5: pour quitter.")
+    if choice == "3": #ok
+        account_choice = input("Quel compte souhaitez vous consulter ? \n 1: Compte courant. \n 2: Compte épargne. \n")
+        if account_choice == "1": #ok
+            Current_account.consult_current(account)
+        if account_choice == "2": #ok
+            Saving_account.consult_saving(account)
+            
+    if choice == "4": #pasok
+        account_choice = input("Que type de virement souhaitez-vous effectuer ? \n 1: Compte courant --> compte épargne. \n 2: Compte épargne --> compte courant. \n")
+        if account_choice =="1": #pasok
+            Current_account.transfer_to_saving(account, amount)
+        if account_choice =="2": #pasok
+            Saving_account.transfer_to_current(account, amount)
+           
+        
+              
+    
